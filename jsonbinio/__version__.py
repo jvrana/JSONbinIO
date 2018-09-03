738795ad43567a9500d84ff91076d78dcff67d52
jvrana <justin.vrana@gmail.com>
2018-09-03 12:51:41 -0700

    version number bump

--- jsonbinio/__version__.py
+++ jsonbinio/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'JSONBinIO'
 __description__ = 'JSONBinIO wrapper'
 __url__ = 'http://www.github.com/jvrana/jsonbinio'
-__version__ = '0.0.1'
+__version__ = '1.0.0'
 __author__ = 'Justin Vrana'
 __author_email__ = "justin.vrana@gmail.com"
